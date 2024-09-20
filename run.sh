2bfb56d17ea1703f2baa5fbe15ec797998a63f43
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-20 20:06:09 +0800

    - update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads:$USERPROFILE/Downloads/qemu-w64-portable-20240822"
 
 qemu-system-aarch64 -M virt -cpu cortex-a72 -nographic -kernel main.elf
+read -p "done"
