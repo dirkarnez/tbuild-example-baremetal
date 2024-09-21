7312c298e7da789866bc62137db1e3735efff492
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-21 23:35:05 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -5,13 +5,12 @@ set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 
 set SEVENZIP=C:\"Program Files"\7-Zip\7z.exe
 set QEMU_DIR=%DOWNLOADS_DIR%\qemu-w64-portable-20240822
-set QEMU_EXE=%QEMU_EXE%\qemu-system-aarch64.exe
+set QEMU_EXE=%QEMU_DIR%\qemu-system-aarch64.exe
 
 if not exist %QEMU_EXE% (
 cd /d "%TEMP%" &&^
 %SystemRoot%\System32\curl.exe "https://github.com/dirkarnez/qemu-portable/releases/download/20240822/qemu-w64-portable-20240822.zip" -L -O  &&^
-%SEVENZIP% x qemu-w64-portable-20240822.zip -o"%QEMU_DIR%" -y &&^
-del qemu-w64-portable-20240822.zip
+%SEVENZIP% x qemu-w64-portable-20240822.zip -o"%QEMU_DIR%" -y
 )
 
 if exist %QEMU_EXE% (
