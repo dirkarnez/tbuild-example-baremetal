9dd7957c29a9c08570057356ae3ce10568208efc
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-21 23:26:03 +0800

    - update and rename run.sh to run.cmd

---
+++ run.cmd
@@ -0,0 +1,20 @@
+@REM run as Administrator
+@echo off
+
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set SEVENZIP=C:\"Program Files"\7-Zip\7z.exe
+set QEMU_DIR=%DOWNLOADS_DIR%\qemu-w64-portable-20240822
+set QEMU_EXE=%QEMU_EXE%\qemu-system-aarch64.exe
+
+if not exist %QEMU_EXE% (
+cd /d "%TEMP%" &&^
+%SystemRoot%\System32\curl.exe "https://github.com/dirkarnez/qemu-portable/releases/download/20231224/qemu-w64-portable-20231224.zip" -L -O  &&^
+%SEVENZIP% x qemu-w64-portable-20231224.zip -o"%QEMU_DIR%" -y &&^
+del qemu-w64-portable-20231224.zip
+)
+
+if exist %QEMU_EXE% (
+  echo qemu %QEMU_EXE% found
+  qemu-system-aarch64 -M virt -cpu cortex-a72 -nographic -kernel main.elf
+)
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads:$USERPROFILE/Downloads/qemu-w64-portable-20240822"
-
-qemu-system-aarch64 -M virt -cpu cortex-a72 -nographic -kernel main.elf
-read -p "done"
